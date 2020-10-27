a8445e0982f3b27358569e84efb41acc51e38b24
Thiago Pires <thiago.pires@sectrans.com.br>
2020-10-27 14:50:38 -0300

    feature - escrita de nested fields

--- demanda/api/serializers.py
+++ demanda/api/serializers.py
@@ -1,9 +1,18 @@
 from demanda.models import Demanda
+from endereco.models import Endereco
+from endereco.api.serializers import EnderecoSerializer
 from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.models import User
+from drf_writable_nested.serializers import WritableNestedModelSerializer
 
 
-class DemandaSerializer(ModelSerializer):
+class DemandaSerializer(WritableNestedModelSerializer):
+    endereco_entrega = EnderecoSerializer()
+    anunciante = User.objects.all()
+
     class Meta:
         model = Demanda
         fields = ('id', 'descricao_peca', 'endereco_entrega', 'informacoes_contato',
                   'anunciante', 'status')
+
+
--- demanda/models.py
+++ demanda/models.py
@@ -3,16 +3,16 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
 from django.db import models
+from endereco.models import Endereco
 
 
 class Demanda(models.Model):
     descricao_peca = models.CharField(max_length=150, null=False, blank=False)
-    endereco_entrega = models.ForeignKey(Endereco, on_delete=models.SET('Endereco Removido'))
+    endereco_entrega = models.ForeignKey(
+        Endereco, on_delete=models.SET('Endereco Removido'))
     informacoes_contato = models.TextField(null=False, blank=False)
     anunciante = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.BooleanField()
 
     def __str__(self):
         return self.descricao_peca
-    
-
