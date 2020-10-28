bb99603743d3f570b0825863c3b25fd3202160e8
piresthiago10 <thiago.pires@sectrans.com.br>
2020-10-27 22:11:50 -0300

    release - autenticacao via token

--- core/urls.py
+++ core/urls.py
@@ -3,12 +3,14 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
 from demanda.api.viewsets import DemandaViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 router = routers.DefaultRouter()
 router.register(r'demandas', DemandaViewSet, basename='Demanda')
 
 urlpatterns = [
+    path('api-token-auth/', obtain_auth_token),
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
