9d8593dcfeb1d0df470bd3e29477e16de9421443
Thiago Pires <thiago.pires@sectrans.com.br>
2020-10-28 16:57:14 -0300

    release - testes

--- demanda/tests.py
+++ demanda/tests.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIRequestFactory, APITestCase
+
+from demanda.api.serializers import DemandaSerializer
+from demanda.models import Demanda
+
+
+class RegistrationTestCase(APITestCase):
+
