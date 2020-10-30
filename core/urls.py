d75bb067ddaf0c068746f6c7af60b4472ce3c008
Thiago Pires <thiago.pires@sectrans.com.br>
2020-10-30 12:29:47 -0300

    feature - servico de media

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
-from django.contrib import admin
+from demanda.api.viewsets import DemandaViewSet
+from django.conf import settings
 from django.conf.urls import include
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
-from demanda.api.viewsets import DemandaViewSet
 from rest_framework.authtoken.views import obtain_auth_token
-
 
 router = routers.DefaultRouter()
 router.register(r'demandas', DemandaViewSet, basename='Demanda')
@@ -13,4 +14,4 @@ urlpatterns = [
     path('api-token-auth/', obtain_auth_token),
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
